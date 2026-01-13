39953a17ef667803b462a882f35db3e67b518d5f
yasmartimiano <yasminliver56@gmail.com>
2026-01-13 17:36:41 -0300

    feat: inicializando o board 3x3 vazio

--- jogodavelha/src/main/java/com/example/Main.java
+++ jogodavelha/src/main/java/com/example/Main.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        char[][] board = new char[3][3];
     }
 }
