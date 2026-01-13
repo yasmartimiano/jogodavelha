6c6fc53a51b87b2f4c261d8c280ee00bf5d42b6c
yasmartimiano <yasminliver56@gmail.com>
2026-01-13 17:42:02 -0300

    feat: adicionando linha e coluna vazias

--- jogodavelha/src/main/java/com/example/Main.java
+++ jogodavelha/src/main/java/com/example/Main.java
@@ -3,5 +3,10 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         char[][] board = new char[3][3];
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[row].length; col++) {
+                board[row][col] = ' ';
+            }
+        }
     }
 }
